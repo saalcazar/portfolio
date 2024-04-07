4c6c4029c640124a26fecbc1f93b19be1dbe27ee
saalcazar <alcazar.samuel@gmail.com>
2024-04-06 22:05:23 -0400

    First Commit

---
+++ src/Routing.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import App from './App'
+import Home from './pages/Home'
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home/>} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default Routing
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="main">
+      <h1 className="kanit-light title">Web programer</h1>
+    </main>
+  )
+}
+
+export default Home
